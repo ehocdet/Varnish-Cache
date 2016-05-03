c7687425b9e9b92f769713d243e0381a1fd3862e
Emmanuel Hocdet <manu@gandi.net>
2016-05-03 14:24:52 +0200

    vcc_err_unref=off shutup
    vcc_unref generate a lot of warning
    it pollute log and slow vcl.load

--- lib/libvcc/vcc_xref.c
+++ lib/libvcc/vcc_xref.c
@@ -104,14 +104,10 @@ vcc_checkref(struct vcc *tl, const struct symbol *sym)
 		VSB_printf(tl->sb, "Undefined %s %.*s, first reference:\n",
 		    VCC_SymKind(tl, sym), PF(sym->def_b));
 		vcc_ErrWhere(tl, sym->def_b);
-	} else if (sym->ndef != 0 && sym->nref == 0) {
+	} else if (sym->ndef != 0 && sym->nref == 0 && tl->param->err_unref) {
 		VSB_printf(tl->sb, "Unused %s %.*s, defined:\n",
 		    VCC_SymKind(tl, sym), PF(sym->def_b));
 		vcc_ErrWhere(tl, sym->def_b);
-		if (!tl->param->err_unref) {
-			VSB_printf(tl->sb, "(That was just a warning)\n");
-			tl->err = 0;
-		}
 	}
 }
 
@@ -282,6 +278,9 @@ vcc_checkaction2(struct vcc *tl, const struct symbol *sym)
 {
 	struct proc *p;
 
+	if (!tl->param->err_unref)
+		return;
+
 	p = sym->proc;
 	AN(p);
 
@@ -289,10 +288,6 @@ vcc_checkaction2(struct vcc *tl, const struct symbol *sym)
 		return;
 	VSB_printf(tl->sb, "Function unused\n");
 	vcc_ErrWhere(tl, p->name);
-	if (!tl->param->err_unref) {
-		VSB_printf(tl->sb, "(That was just a warning)\n");
-		tl->err = 0;
-	}
 }
 
 int

cfcda4c807491a8523fbe0f54a3e7312a550cc04
Emmanuel Hocdet <manu@gandi.net>
2019-04-16 17:58:43 +0200

    remove assert for #2572

--- bin/varnishd/http2/cache_http2_proto.c
+++ bin/varnishd/http2/cache_http2_proto.c
@@ -772,7 +772,7 @@ h2_rx_data(struct worker *wrk, struct h2_sess *h2, struct h2_req *r2)
 	Lck_Lock(&h2->sess->mtx);
 	while (h2->mailcall != NULL && h2->error == 0 && r2->error == 0)
 		AZ(Lck_CondWait(h2->cond, &h2->sess->mtx, 0));
-	AZ(h2->mailcall);
+	//	AZ(h2->mailcall);
 	h2->mailcall = r2;
 	h2->req0->r_window -= h2->rxf_len;
 	r2->r_window -= h2->rxf_len;
